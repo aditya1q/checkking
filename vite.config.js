9916e78b9330613034d8d2fb7121608787cc42f1
aditya1q <kanhatiwari9410@gmail.com>
2024-12-17 16:33:31 +0530

    Resolved merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Adjust if deploying to a subdirectory
 })
